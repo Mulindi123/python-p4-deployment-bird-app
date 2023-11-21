055c7643fef477bcc6fec359662c12e05c8d2202
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-11-21 13:51:20 +0300

    changes

--- server/app.py
+++ server/app.py
@@ -16,7 +16,7 @@ app = Flask(
     template_folder='../client/build'
 )
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI') #from .env
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
